8fe8aedf356229e87241f54670f828bccb9431a2
Erika Haren <emharen@hotmail.com>
2019-06-09 15:36:36 -0400

    update

--- logic.js
+++ logic.js
@@ -36,17 +36,17 @@ function createMap(tract) {
     accessToken: API_KEY
   });
 
-  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.dark",
+    id: "mapbox.light",
     accessToken: API_KEY
   });
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     "Street Map": streetmap,
-    "Dark Map": darkmap
+    "Light Map": lightmap
   };
 
   // Create overlay object to hold our overlay layer
@@ -59,7 +59,7 @@ function createMap(tract) {
     center: [
       41.881832, -87.623177
     ],
-    zoom: 10,
+    zoom: 9.5,
     layers: [streetmap, tract]
   });
 
