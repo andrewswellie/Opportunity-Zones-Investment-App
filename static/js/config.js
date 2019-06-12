bcb8baa66dcfdc63cab25900b5aee0698aeb6b17
Andrew Swellie <andrewswellie@gmail.com>
2019-06-12 11:47:56 -0400

    ...

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiZW1oYXJlbiIsImEiOiJjandiYWpsMHkwaXB6NDZxbnRiejBwbTZhIn0.CPgdwiWMEy4OFDdkCTMwEg";
+const API_KEY = "pk.eyJ1IjoiYW5kcmV3c3dlbGxpZSIsImEiOiJjandiOHViNWQwaTRpM3pvNmxnajRuZTFqIn0.W8y5QyayytNoeN9SiYPF-Q";
