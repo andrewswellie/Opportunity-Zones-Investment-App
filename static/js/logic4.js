c58ca21d16c41571cc9962d13fdd6bd3ce442b7e
Andrew Swellie <andrewswellie@gmail.com>
2019-06-14 18:05:37 -0400

    .

--- static/js/logic4.js
+++ static/js/logic4.js
@@ -17,20 +17,20 @@ return rows.map(function(row) {
 
 function buildPlot() {
     d3.csv("https://raw.githubusercontent.com/andrewswellie/ProjectTwo_Opportunity-Zones-Investment-App/master/csv_raw_data/final_api_data.csv", function(demoData) {
-        // console.log(demoData);
+        console.log(demoData);
 
-        demoData.forEach(function(data) {
-            data.tract = +data.tract;
-            data.median_income = +data.median_income;
+        // demoData.forEach(function(data) {
+        //     data.tract = +data.tract;
+        //     data.median_income = +data.median_income;
             
           });
 
   // Grab values from the data json object to build the plots
-  console.log(demoData);
-  console.log(data);
+//   console.log(demoData);
+//   console.log(data);
   
-  var tract = data.tract;
-  var income = data.median_income;
+  var tract = demoData.tract;
+  var income = demoData.median_income;
 
   console.log(tract);
   console.log(income);
