54dc4dba09cd7e47ef3447bf1c96d91507dc38b9
Andrew Swellie <andrewswellie@gmail.com>
2019-06-14 18:20:59 -0400

    .

--- static/js/logic4.js
+++ static/js/logic4.js
@@ -1,14 +1,3 @@
-/**
-* Helper function to select stock data
-* Returns an array of values
-* @param {array} rows
-* @param {integer} index
-* index 0 - Date
-* index 1 - Open
-* index 2 - High
-* index 3 - Low
-* index 4 - Volume
-*/
 function unpack(rows, index) {
 return rows.map(function(row) {
   return row[index];
@@ -45,7 +34,7 @@ function buildPlot() {
 
   Plotly.newPlot("graph", data);
 
-};
+});
 
 
-buildPlot();
+buildPlot();}
