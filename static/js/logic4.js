381cf06e9825c96ff407bd06d043849a166a9f8b
Andrew Swellie <andrewswellie@gmail.com>
2019-06-14 13:59:11 -0400

    .

--- static/js/logic4.js
+++ static/js/logic4.js
@@ -37,34 +37,34 @@
   
       Plotly.newPlot("bubble", data, layout, {scrollZoom: true});
     });
-  
+}
       
   
-  function init() {
-        // Grab a reference to the dropdown select element
-        var selector = d3.select("#selDataset");
+//   function init() {
+//         // Grab a reference to the dropdown select element
+//         var selector = d3.select("#selDataset");
   
-        // Use the list of sample names to populate the select options
-        d3.json("/names").then((sampleNames) => {
-          sampleNames.forEach((sample) => {
-            selector
-              .append("option")
-              .text(sample)
-              .property("value", sample);
-          });
+//         // Use the list of sample names to populate the select options
+//         d3.json("/names").then((sampleNames) => {
+//           sampleNames.forEach((sample) => {
+//             selector
+//               .append("option")
+//               .text(sample)
+//               .property("value", sample);
+//           });
   
-          // Use the first sample from the list to build the initial plots
-          const firstSample = sampleNames[0];
-          buildCharts(firstSample);
-          buildMetadata(firstSample);
-        });
-      }
+//           // Use the first sample from the list to build the initial plots
+//           const firstSample = sampleNames[0];
+//           buildCharts(firstSample);
+//           buildMetadata(firstSample);
+//         });
+//       }
   
-  function optionChanged(newSample) {
-        // Fetch new data each time a new sample is selected
-        buildCharts(newSample);
-        buildMetadata(newSample);
-      }
+//   function optionChanged(newSample) {
+//         // Fetch new data each time a new sample is selected
+//         buildCharts(newSample);
+//         buildMetadata(newSample);
+//       }
   
-  // Initialize the dashboard
-  init();
+//   // Initialize the dashboard
+//   init();
