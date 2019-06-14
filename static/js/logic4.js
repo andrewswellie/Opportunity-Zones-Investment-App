c3686332c9fbd7a18197e17be3ec2842b44ec023
Andrew Swellie <andrewswellie@gmail.com>
2019-06-14 14:10:48 -0400

    .

--- static/js/logic4.js
+++ static/js/logic4.js
@@ -1,70 +1,47 @@
 
- function buildCharts(sample) {
-  
-    // @TODO: Use `d3.json` to fetch the sample data for the plots
-    var url = (`/api/v1.0`)
+ /* global Plotly */
+var url = "https://cwru-opportunity-zones-tool.herokuapp.com/api/v1.0";
 
-    // @TODO: Build a Bubble Chart using the sample data
-    d3.json(url).then(function (data) {
-      var tract = data['census_tract'];
-      var income = data['median_income'];
-      
-  
-      var trace = {
-        x: tract,
-        y: income,
-        type: "scatter",
-        mode: "markers",
-        marker: {
-          color: tract,
-          size: income,
-          symbol: "circle"
-        },
-        text: "Hello, test",
-        textinfo: 'none',
-        hoverinfo: "x+y+text"
-      };
-  
-      var data = [trace];
-  
-      var layout = {
-        title: "Bubble Chart",
-        xaxis: { title: "Census Tract ID" },
-        yaxis: { title: "Median Household Income" },
-        hovermode: "closest",
-        margin: { t:0 }
-      };
-  
-      Plotly.newPlot("bubble", data, layout, {scrollZoom: true});
-    });
+/**
+* Helper function to select stock data
+* Returns an array of values
+* @param {array} rows
+* @param {integer} index
+* index 0 - Date
+* index 1 - Open
+* index 2 - High
+* index 3 - Low
+* index 4 - Volume
+*/
+function unpack(rows, index) {
+return rows.map(function(row) {
+  return row[index];
+});
 }
-      
+
+function buildPlot() {
+d3.json(url).then(function(data) {
+
+  // Grab values from the data json object to build the plots
+  var tract = data.dataset.census_tract;
+  var income = data.dataset.median_income;
   
-//   function init() {
-//         // Grab a reference to the dropdown select element
-//         var selector = d3.select("#selDataset");
-  
-//         // Use the list of sample names to populate the select options
-//         d3.json("/names").then((sampleNames) => {
-//           sampleNames.forEach((sample) => {
-//             selector
-//               .append("option")
-//               .text(sample)
-//               .property("value", sample);
-//           });
-  
-//           // Use the first sample from the list to build the initial plots
-//           const firstSample = sampleNames[0];
-//           buildCharts(firstSample);
-//           buildMetadata(firstSample);
-//         });
-//       }
-  
-//   function optionChanged(newSample) {
-//         // Fetch new data each time a new sample is selected
-//         buildCharts(newSample);
-//         buildMetadata(newSample);
-//       }
-  
-//   // Initialize the dashboard
-//   init();
+  var trace1 = {
+    type: "scatter",
+    mode: "lines",
+    name: tract,
+    x: tract,
+    y: income,
+    line: {
+      color: "#17BECF"
+    }
+  };
+
+  var data = [trace1];
+
+  Plotly.newPlot("plot", data);
+
+});
+}
+
+buildPlot();
