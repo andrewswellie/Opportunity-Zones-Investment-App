4727bc8eca2721a3d278f77f5c5b61d0c3400dfe
Andrew Swellie <andrewswellie@gmail.com>
2019-06-15 16:40:43 -0400

    .

--- static/js/logic.js
+++ static/js/logic.js
@@ -42,7 +42,7 @@ function createFeatures(tracts) {
             });
           },
           click: function(event) {
-            map.fitBounds(event.target.getBounds());
+            map.flyToBounds(event.target.getBounds());
           }
         });
            
