7b2b11abdf297e01554f4426d3676890ff66ac0d
Andrew Swellie <andrewswellie@gmail.com>
2019-06-15 20:18:04 -0400

    .

--- static/js/logic.js
+++ static/js/logic.js
@@ -91,9 +91,9 @@ function createMap(tract) {
   // Create our map, giving it the streetmap and tract layers to display on load
   var myMap = L.map("map", {
     center: [
-      41.881832, -87.623177
+      41.812779, -87.756042
     ],
-    zoom: 10.45,
+    zoom: 14,
     layers: [streetmap, tract]
   });
 
